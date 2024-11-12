a5b4d5c55939bc35f02698a6a3a6f953750f9ae3
GNC <145757613+GMF21@users.noreply.github.com>
2024-11-12 21:13:52 -0100

    Update mota.h

--- mota.h
+++ mota.h
@@ -1,1 +1,15 @@
+#ifndef MOTA_H
+#define MOTA_H
+
 #include "veiculo.h"
+
+class Mota : public Veiculo {
+    private:
+        string cc;
+    
+    public:
+        Mota(string name, string cc);
+        void mostrarDetalhes() override;
+};
+
+#endif 
