1dc999fabe26f9efb4858c6b198eca150db30922
GNC <145757613+GMF21@users.noreply.github.com>
2024-11-12 21:13:08 -0100

    Update car.h

--- car.h
+++ car.h
@@ -1,1 +1,15 @@
+#ifndef CAR_H
+#define CAR_H
+
 #include "veiculo.h"
+
+class Car : public Veiculo {
+    private: 
+        int num_portas;
+
+    public:
+        Car(string name, int num_portas);
+        void mostrarDetalhes() override;
+};
+
+#endif 
