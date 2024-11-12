083c7518449dbc6b0574207debedf326f4fd9762
GNC <145757613+GMF21@users.noreply.github.com>
2024-11-12 21:12:52 -0100

    Update car.cpp

--- car.cpp
+++ car.cpp
@@ -1,3 +1,10 @@
-#include <iostream>
-#include "car.h"
-using namespace std;
+#include "Car.h"
+
+Car::Car(string Name, int portas) : Veiculo(Name) {
+    num_portas = portas;
+}
+
+void Car::mostrarDetalhes() {
+    Veiculo::mostrarDetalhes();
+    cout << "Numero de portas: " << num_portas << endl;
+}
